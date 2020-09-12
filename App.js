0c39165d696b170c31df880692392cc5774fe6fa
Mauricio Miramontes <mmiramontesr@gmail.com>
2020-09-11 21:40:07 -0500

    Navigation Part 1

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Main from './components/MainComponent';
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Remote debugger']);
+
 
 export default class App extends React.Component {
   render() {
