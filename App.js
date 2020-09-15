2a36200a5df223416258ac9e3224cc97cea254a0
Mauricio Miramontes <mmiramontesr@gmail.com>
2020-09-14 22:58:52 -0500

    Persist Redux Store

--- App.js
+++ App.js
@@ -4,15 +4,21 @@ import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Remote debugger']);
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { Loading } from './components/LoadingComponent';
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Main />
+        <PersistGate
+          loading={<Loading />}
+          persistor={persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
